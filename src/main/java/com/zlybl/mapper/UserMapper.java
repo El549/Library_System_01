ab956e3170fe5a05bb9ad3df7d71405f47f81b02
HaoWish <77219072+HaoWish@users.noreply.github.com>
2022-10-07 15:56:59 +0800

    UserMapper添加了查找，修改、添加、删除功能

--- src/main/java/com/zlybl/mapper/UserMapper.java
+++ src/main/java/com/zlybl/mapper/UserMapper.java
@@ -1,4 +1,21 @@
 package com.zlybl.mapper;
 
+import com.zlybl.pojo.User;
+
+import java.util.List;
+
 public interface UserMapper {
+    //查询所有用户
+    List<User> selectAllUser();
+    //根据多个条件查询一组用户
+    List<User> selectUsersByConditions(User user);
+    //根据多个条件查询单个用户
+    User selectUserByConditions(User user);
+    //添加用户
+    int addUser(User user);
+    //修改用户信息
+    int updateUser(User user);
+    //删除用户
+    int deleteUser(int userId);
+
 }
